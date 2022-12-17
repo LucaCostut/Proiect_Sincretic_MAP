378a2d9f868f509462ca12d860b3947187a6c61f
LucaCostut <115777986+LucaCostut@users.noreply.github.com>
2022-12-17 21:25:44 +0200

    Am facut functia in care calculam solutiile dar nu merge

--- CODE.cpp
+++ CODE.cpp
@@ -26,10 +26,44 @@ void Afisare(int tabla[linMax][colMax])
     cout<<"\n<------------------>\n\n";
 }
 
+
+void fct(int tabla[linMax][colMax],int lin,int &sol)
+{
+    if(lin==linMax)
+    {
+        Afisare(tabla); //Se apeleaza functia de afisare a tablei atunci cand toate cele 8 ture au fost amplasate
+        sol++; //Se incrementeaza numarul de solutii     
+    }
+    else{
+        for(int col=0;col<colMax;col++)
+            if(tabla[lin][col]==0)
+            {
+                for (int i = 0; i < linMax; i++)
+                    if(i == lin)
+                        continue;
+                    else
+                        tabla[i][col]= -1;
+                fct(tabla,lin+1,sol);
+                for (int i = 0; i < linMax; i++)
+                    tabla[i][col] = 0;
+                
+
+                
+                
+            }
+    }
+}
+
 int main ()
 {
     return 0;
 
-    int mama;
+    int tabla[linMax][colMax];
+    int lin=0;
+    int sol=0;
+    InitializareTabla(tabla);
+    fct(tabla,lin,sol);
+
+    cout<<sol;
     
 }
