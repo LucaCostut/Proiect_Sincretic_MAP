9f03672c45703ede09d0a1ef22a792379e48ced4
LucaCostut <115777986+LucaCostut@users.noreply.github.com>
2022-12-17 21:33:26 +0200

    Programul mergea dar era return 0 inaintea functiilor

--- CODE.cpp
+++ CODE.cpp
@@ -17,10 +17,12 @@ void Afisare(int tabla[linMax][colMax])
     {
         cout<<" ";
         for(int col=0;col<colMax;col++)
+         {
             if(tabla[lin][col] == -1)
                 cout<<"* ";
             else
                 cout<<"@ ";
+         }
         cout<<"\n";
     }
     cout<<"\n<------------------>\n\n";
@@ -34,29 +36,26 @@ void fct(int tabla[linMax][colMax],int lin,int &sol)
         Afisare(tabla); //Se apeleaza functia de afisare a tablei atunci cand toate cele 8 ture au fost amplasate
         sol++; //Se incrementeaza numarul de solutii     
     }
-    else{
+    else
+    {
         for(int col=0;col<colMax;col++)
             if(tabla[lin][col]==0)
             {
                 for (int i = 0; i < linMax; i++)
-                    if(i == lin)
+                {   if(i == lin)
                         continue;
                     else
                         tabla[i][col]= -1;
-                fct(tabla,lin+1,sol);
+                }
+                fct(tabla,lin + 1,sol);
                 for (int i = 0; i < linMax; i++)
                     tabla[i][col] = 0;
-                
-
-                
-                
             }
     }
 }
 
 int main ()
 {
-    return 0;
 
     int tabla[linMax][colMax];
     int lin=0;
