657a973c4e0bc4f929d8bda2fd4910b9f6c05f61
LucaCostut <115777986+LucaCostut@users.noreply.github.com>
2022-12-17 21:14:43 +0200

    Am facut functia de afisare

--- CODE.cpp
+++ CODE.cpp
@@ -3,14 +3,28 @@ using namespace std;
 
 #define linMax 8
 #define colMax 8
-
+//Functia de initializare cu 0 pe toate pozitiile.
 void InitializareTabla(int tabla[linMax][colMax])
 {
     for (int lin=0;lin<linMax;lin++)
         for(int col=0;col<colMax;col++)
             tabla[lin][col]=0;
 }
-
+//Functia de afisare.
+void Afisare(int tabla[linMax][colMax])
+{
+    for(int lin=0;lin<linMax;lin++)
+    {
+        cout<<" ";
+        for(int col=0;col<colMax;col++)
+            if(tabla[lin][col] == -1)
+                cout<<"* ";
+            else
+                cout<<"@ ";
+        cout<<"\n";
+    }
+    cout<<"\n<------------------>\n\n";
+}
 
 int main ()
 {
